4806167c4fb1ed5b94449b9a4782b83ca6a0f842
Chris Hagan <chris.hagan@monash.edu>
2010-05-04 14:50:23 +1000

    Added sandpit
    Former-commit-id: ca02eb23bede4b5fc7ba4ba74afe05d7f1a0b4b9

---
+++ MeTLMeeting/SandRibbon/Components/Sandpit/BoardManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SandRibbon.Components.Sandpit
+{
+    public class BoardManager
+    {
+        public static Dictionary<string, IEnumerable<string>> boards = new Dictionary<string,IEnumerable<string>>
+        { 
+            {"S15",Enumerable.Range(1,7).Select(i=>string.Format("S15{0}",i)).ToList()}
+        };
+    }
+}
