cda00449a4890fef6c43dfb719c7cf703c8be25b
Chris Hagan <chris.hagan@monash.edu>
2010-05-13 10:46:13 +1000

    Added the sandpitty stuff about bubbles
    Former-commit-id: 3caf2ff86df505bd3e9062d6079a5bd878194c77

---
+++ MeTLMeeting/SandRibbon/Components/Sandpit/ThoughtBubble.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SandRibbonInterop.MeTLStanzas;
+using System.Windows.Ink;
+
+namespace SandRibbon.Components.Sandpit
+{
+    public partial class ThoughtBubble : UserControl
+    {
+        public Point position = new Point(0,0);
+        public int parent;
+        public List<Stroke> strokeContext;
+        public List<FrameworkElement> childContext;
+        public ThoughtBubble()
+        {
+            InitializeComponent();
+            strokeContext = new List<Stroke>();
+            childContext = new List<FrameworkElement>();
+        }
+        public ThoughtBubble relocate() 
+        {
+            var bounds = getBounds();
+            position = new Point(bounds.X, bounds.Y);
+            return this;
+        }
+        private Rect getBounds()
+        {
+            var listX = new List<Double>();
+            var listY = new List<Double>();
+            foreach (Stroke stroke in strokeContext)
+            {
+                listX.Add(stroke.GetBounds().Left);
+                listY.Add(stroke.GetBounds().Top);
+            }
+            foreach (var child in childContext)
+            {
+                listX.Add(InkCanvas.GetLeft(child));
+                listY.Add(InkCanvas.GetTop(child));
+            }
+            if (listX.Count > 0)
+                return new Rect
+                {
+                    X = listX.Min(),
+                    Y = listY.Min(),
+                    Width = listX.Max() - listX.Min(),
+                    Height = listY.Max() - listY.Min()
+                };
+            else
+                return new Rect();
+        }
+    }
+}
---
+++ MeTLMeeting/SandRibbon/Components/Sandpit/ThoughtBubbleLauncher.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Practices.Composite.Presentation.Commands;
+using SandRibbonInterop.MeTLStanzas;
+
+namespace SandRibbon.Components.Sandpit
+{
+    public partial class ThoughtBubbleLauncher : UserControl
+    {
+        private SandRibbon.Utils.Connection.JabberWire.Credentials credentials;
+        private string privacy;
+        private int currentSlide;
+        public ThoughtBubbleLauncher()
+        {
+            InitializeComponent();
+            Commands.BubbleCurrentSelection.RegisterCommand(new DelegateCommand<object>(BubbleCurrentSelection));
+            Commands.SetIdentity.RegisterCommand(new DelegateCommand<SandRibbon.Utils.Connection.JabberWire.Credentials>(SetIdentity));
+            Commands.SetPrivacy.RegisterCommand(new DelegateCommand<string>(SetPrivacy));
+            Commands.MoveTo.RegisterCommand(new DelegateCommand<int>(MoveTo));
+        }
+        private void SetIdentity(SandRibbon.Utils.Connection.JabberWire.Credentials credentials){
+            this.credentials = credentials;
+        }
+        private void SetPrivacy(string privacy){
+            this.privacy = privacy;
+        }
+        private void MoveTo(int where) {
+            this.currentSlide = where;
+        }
+        private void BubbleCurrentSelection(object _nothing) 
+        {
+            string target = null;
+            var selection = new List<SelectedIdentity>();
+            foreach(var registeredCommand in Commands.DoWithCurrentSelection.RegisteredCommands)
+                registeredCommand.Execute((Action<SelectedIdentity>)(id=>{
+                    target = id.target;
+                    selection.Add(id);
+                }));
+            if (selection.Count() > 0)
+                Commands.SendNewBubble.Execute(new TargettedBubbleContext
+                {
+                    author = credentials.name,
+                    context = selection,
+                    privacy = "public",
+                    slide = currentSlide,
+                    target = target
+                });
+        } 
+    }
+}
