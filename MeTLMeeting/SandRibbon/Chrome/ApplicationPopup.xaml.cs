5c108a1e8649dd1afd1e8dc384682750176eb315
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-05-24 15:02:51 +1000

    fucking new files
    Former-commit-id: 2756b1232e10d71680965ee228fa4bcc31d3ae3d

---
+++ MeTLMeeting/SandRibbon/Chrome/ApplicationPopup.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Practices.Composite.Presentation.Commands;
+using SandRibbonObjects;
+using SandRibbon.Providers;
+using SandRibbon.Providers.Structure;
+
+namespace SandRibbon.Chrome
+{
+    public partial class ApplicationPopup : Divelements.SandRibbon.ApplicationPopup
+    {
+        public ApplicationPopup()
+        {
+            InitializeComponent();
+            Opened += ApplicationButtonPopup_Opened;
+            Closed += ApplicationButtonPopup_Closed;
+        }
+        private void ApplicationButtonPopup_Closed(object sender, EventArgs e)
+        {
+            Commands.SetTutorialVisibility.Execute(Visibility.Collapsed);
+        }
+        private void ApplicationButtonPopup_Opened(object sender, EventArgs e)
+        {
+            Commands.SetTutorialVisibility.Execute(Visibility.Visible);
+        }
+        #region helpLinks
+        private void OpenEULABrowser(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://metl.adm.monash.edu.au/MeTL/docs/tabletSupport/MLS_UserAgreement.html");
+        }
+        private void OpenTutorialBrowser(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://metl.adm.monash.edu.au/MeTL/docs/tabletSupport/MLS_Tutorials.html");
+        }
+        private void OpenReportBugBrowser(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://metl.adm.monash.edu.au/MeTL/docs/report_a_bug.html");
+        }
+        private void OpenAboutMeTLBrowser(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.monash.edu.au/eeducation/myls2010/students/resources/software/metl/");
+        }
+        #endregion
+    }
+}
