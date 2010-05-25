816a0982bb387894ae92d1ec55c6d5de912fe0b1
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-05-25 16:22:02 +1000

    adding files
    Former-commit-id: f3c36ad8e5fe07159c349b5ebe193986891906a9

---
+++ MeTLMeeting/SandRibbon/Chrome/StatusBar.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Practices.Composite.Presentation.Commands;
+using SandRibbon.Providers;
+using SandRibbonObjects;
+using System.Threading;
+
+namespace SandRibbon.Chrome
+{
+    public partial class StatusBar : Divelements.SandRibbon.StatusBar
+    {
+        public StatusBar()
+        {
+            InitializeComponent();
+            Commands.SetPrivacy.RegisterCommand(new DelegateCommand<string>(SetPrivacy));
+            Commands.JoinConversation.RegisterCommand(new DelegateCommand<string>(JoinConversation));
+            Commands.UpdateConversationDetails.RegisterCommand(new DelegateCommand<ConversationDetails>(UpdateConversationDetails));
+        }
+        private void SetPrivacy(string _privacy)
+        {
+            showDetails();
+        }
+        private void JoinConversation(string _jid)
+        {
+            showDetails();
+        }
+        private void UpdateConversationDetails(ConversationDetails _details) 
+        { 
+            showDetails();
+        }
+        private void showDetails()
+        {
+            try
+            {
+                var details = Globals.conversationDetails;
+                var doDetails = (Action)delegate
+                                             {
+                                                 StatusLabel.Text = string.Format("{3} is working {0}ly in {1} style, in a conversation whose participants are {2}",
+                                                         Globals.privacy, Permissions.InferredTypeOf(details.Permissions).Label, details.Subject, Globals.me);
+                                             };
+                if (Thread.CurrentThread != Dispatcher.Thread)
+                    Dispatcher.BeginInvoke(doDetails);
+                else
+                    doDetails();
+            }
+            catch (NotSetException) { }
+        }
+    }
+}
