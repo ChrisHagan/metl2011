dfaa127059ab855eb152a89e1978c0a16847f61b
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-05-25 16:23:45 +1000

    add
    Former-commit-id: 42808520de29cc84b193e385737c5b431e6c6ff6

---
+++ MeTLMeeting/SandRibbon/Components/Pedagogicometry/PedagogyLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SandRibbon.Components.Pedagogicometry
+{
+    public class PedagogyLevel
+    {
+        public int code;
+        public string label { get; set; }
+    }
+}
