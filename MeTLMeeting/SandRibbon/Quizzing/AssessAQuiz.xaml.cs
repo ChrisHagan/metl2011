3f3e156f4fa486962abacf1e80468bd4605903b6
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-06-01 14:05:58 +1000

    adding assess a quiz
    Former-commit-id: b287839650f9ac5515696dff2265814cbdf4eb43

---
+++ MeTLMeeting/SandRibbon/Quizzing/AssessAQuiz.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SandRibbonInterop;
+
+namespace SandRibbon.Quizzing
+{
+    public partial class AssessAQuiz : Window
+    {
+        public AssessAQuiz()
+        {
+            InitializeComponent();
+        }
+        public AssessAQuiz(List<QuizAnswer> answers, QuizQuestion question) : this() 
+        {
+            resultDisplay.DataContext = question.options.Select(o =>{
+                var relevant = answers.Where(a=>a.answer==o.name);
+                    return new DisplayableResultSet
+                    {
+                        color = o.color,
+                        count = relevant.Count(),
+                        percentage = relevant.Count() / answers.Count() * 100,
+                        tooltip = o.optionText
+                    };
+                }
+            );
+            allResults.DataContext = question;
+        }
+    }
+    public class DisplayableResultSet 
+    {
+        public Color color { get; set; }
+        public int count { get; set; }
+        public int percentage { get; set; }
+        public string tooltip { get; set; }
+        public string label {
+            get {
+                return string.Format("{0}% ({1} responses)", percentage, count);
+            }
+        }
+    }
+}
