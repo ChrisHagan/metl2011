d35455ddcdd92998078cba35fad59de5d067802b
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-06-03 12:08:29 +1000

    Added some missing stuff
    Former-commit-id: 8ab2bb056e317c0d712db2a4e43ac3e560eae56a

---
+++ MeTLMeeting/SandRibbon/Components/HelpButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SandRibbon.Components
+{
+    public partial class HelpButton : Button
+    {
+        public HelpButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
