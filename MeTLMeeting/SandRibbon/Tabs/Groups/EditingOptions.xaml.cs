26088e3b4341137e5ce91ab3f3025aad26223162
David <David Hagan (david.hagan@monash.edu)>
2010-07-29 14:06:16 +1000

    forgot to add the new group
    Former-commit-id: 4b27ac1f621fc9190aa66cade020c8a3a7df8cce

---
+++ MeTLMeeting/SandRibbon/Tabs/Groups/EditingOptions.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Divelements.SandRibbon;
+using Microsoft.Practices.Composite.Presentation.Commands;
+
+namespace SandRibbon.Tabs.Groups
+{
+    public partial class EditingOptions : RibbonGroup
+    {
+        public EditingOptions()
+        {
+            InitializeComponent();
+            Commands.SetLayer.RegisterCommand(new DelegateCommand<string>(switchTools));
+        }
+        private void switchTools(string mode)
+        {
+            hideAll();
+            this.Visibility = Visibility.Visible;
+            switch (mode)
+            {
+                case "Sketch":
+                    penColors.Visibility = Visibility.Visible;
+                    Header = "Drawing Tools";
+                    break;
+                case "Text":
+                    toolBox.Visibility = Visibility.Visible;
+                    textTools.Visibility = Visibility.Visible;
+                    Header = "Writing Tools";
+                    break;
+                case "Insert":
+                    toolBox.Visibility = Visibility.Visible;
+                    Header = "Image Tools";
+                    break;
+                case "View":
+                    Header = "View Tools";
+                    this.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+        private void hideAll() {
+            foreach (FrameworkElement child in LayoutRoot.Children)
+            {
+                child.Visibility = Visibility.Collapsed;
+            }
+        }
+
+    }
+}
