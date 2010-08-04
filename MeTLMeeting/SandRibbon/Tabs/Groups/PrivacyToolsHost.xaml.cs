7bae4409c0d471c707bfe6eab46225e3bd08c1bd
David <David Hagan (david.hagan@monash.edu)>
2010-08-04 12:25:48 +1000

    added privacyToolsHost
    Former-commit-id: 892620d5d2c3e3af325f382f706616360185b8d1

---
+++ MeTLMeeting/SandRibbon/Tabs/Groups/PrivacyToolsHost.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Divelements.SandRibbon;
+using Microsoft.Practices.Composite.Presentation.Commands;
+using SandRibbon.Providers.Structure;
+using SandRibbon.Providers;
+
+namespace SandRibbon.Tabs.Groups
+{
+    public partial class PrivacyToolsHost : RibbonGroup
+    {
+        public PrivacyToolsHost()
+        {
+            InitializeComponent();
+            Commands.JoinConversation.RegisterCommand(new DelegateCommand<string>(updatePrivacyTools));
+        }
+        private void updatePrivacyTools(string jid)
+        {
+            try
+            {
+                if (new FileConversationDetailsProvider().DetailsOf(jid).Author == Globals.me || Globals.pedagogy.code > 2)
+                    //ribbonPrivacyTools.Visibility = Visibility.Visible;
+                    this.Visibility = Visibility.Visible;
+                else {
+                    //ribbonPrivacyTools.Visibility = Visibility.Collapsed; 
+                    this.Visibility = Visibility.Collapsed; 
+                }
+            }
+            catch (Exception ex) { }
+        }
+     }
+}
