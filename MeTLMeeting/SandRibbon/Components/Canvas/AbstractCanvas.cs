ff7306729ec9129f04ded6d54a4b2002937ca96d
David <David Hagan (david.hagan@monash.edu)>
2010-09-03 16:47:28 +1000

    slight update to the insertion of multiple pictures as a grid to neaten up the horizontal spacing and the position of the start of the newline
    Former-commit-id: 51f25bc5a9bb100bd5ae03aabb4898d0ed5289b8

--- MeTLMeeting/SandRibbon/Components/Canvas/AbstractCanvas.cs
+++ MeTLMeeting/SandRibbon/Components/Canvas/AbstractCanvas.cs
@@ -233,6 +233,7 @@ namespace SandRibbon.Components.Canvas
             }
             Commands.SetLayer.Execute("Insert");
             var pos = e.GetPosition(this);
+            var origX = pos.X;
             //lets try for a 4xN grid
             var height = 0.0;
             for (var i = 0; i < fileNames.Count(); i++)
@@ -240,11 +241,11 @@ namespace SandRibbon.Components.Canvas
                 var filename = fileNames[i];
                 var image = Image.createImageFromUri(new Uri(filename, UriKind.RelativeOrAbsolute));
                 Commands.ImageDropped.Execute(new ImageDrop { filename = filename, point = pos, target = target, position = i });
-                pos.X += image.Width;
+                pos.X += image.Width + 30;
                 if (image.Height > height) height = image.Height;
                 if ((i + 1) % 4 == 0)
                 {
-                    pos.X = 0;
+                    pos.X = origX;
                     pos.Y += (height + 30);
                     height = 0.0;
                 }
