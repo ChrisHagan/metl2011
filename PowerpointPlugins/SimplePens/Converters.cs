0eb1b0360a29cf834b6bac422506ae24b377ef32
David <David Hagan (david.hagan@monash.edu)>
2010-09-09 17:01:34 +1000

    SimplePens ready to publish
    Former-commit-id: bbf46511585aa747f9d24bbd188b84613097884e

---
+++ PowerpointPlugins/SimplePens/Converters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace PowerpointJabber
+{
+    class Converters
+    {
+        public static BoolToVisibilityConverter boolToVisibilityConverter = new BoolToVisibilityConverter();
+        public static ReverseBoolToVisibilityConverter reverseBoolToVisibilityConverter = new ReverseBoolToVisibilityConverter();
+
+        public class BoolToVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            }
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class ReverseBoolToVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if ((bool)value)
+                {
+                    return Visibility.Collapsed;
+                }
+                return Visibility.Visible;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                if ((Visibility)value == Visibility.Visible)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    
+    }
+}
---
+++ PowerpointPlugins/SimplePens/WindowsInteropFunctions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace PowerpointJabber
+{
+    class WindowsInteropFunctions
+    {
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("User32.dll", EntryPoint = "ShowWindowAsync")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        private const int WS_SHOWNORMAL = 1;
+        private const int WS_SHOWMAXIMIZED = 3;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // Find window by Caption only. Note you must pass IntPtr.Zero as the first parameter.
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        private static IntPtr FindWindowByCaption(string lpWindowName)
+        {
+            return FindWindowByCaption(IntPtr.Zero, lpWindowName);
+        }
+
+        public static void BringAppropriateViewToFront()
+        {
+            var PresenterWindow = FindWindowByCaption("PowerPoint Presenter View - ["+ThisAddIn.instance.Application.ActivePresentation.Windows[1].Caption+"]");
+            if ((int)PresenterWindow > 0)
+                BringWindowToFront(PresenterWindow);
+            else
+                BringWindowToFront((IntPtr)ThisAddIn.instance.Application.ActivePresentation.SlideShowWindow.HWND);
+        }
+
+        public static void BringWindowToFront(IntPtr windowHandle)
+        {
+            SystemParametersInfo((uint)0x2001, 0, 0, 0x0002 | 0x0001);
+            ShowWindowAsync(windowHandle, WS_SHOWMAXIMIZED);
+            SetForegroundWindow(windowHandle);
+            SystemParametersInfo((uint)0x2001, 200000, 200000, 0x0002 | 0x0001);
+        }
+    }
+}
