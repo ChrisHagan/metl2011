f6ffd5ec53bfc1570f8860c1220beb7a692d0496
Chris Hagan <chris.hagan@monash.edu>
2010-11-09 10:45:37 +1100

    UserOptions.
    Former-commit-id: 471cb19b00ebba6fa217ae605842220d02e4b2ff

---
+++ MeTLMeeting/MeTLLib/DataTypes/UserOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace MeTLLib.DataTypes
+{
+    public class UserOptions
+    {
+        public int pedagogyLevel { get; set; }
+        public int powerpointImportScale { get; set; }
+        public string logLevel { get; set; }
+        public static UserOptions DEFAULT = new UserOptions { 
+            logLevel = "ERROR",
+            pedagogyLevel = 2,
+            powerpointImportScale = 1
+        };
+        public static UserOptions ReadXml(string xml) {
+            return (UserOptions) new XmlSerializer(typeof(UserOptions)).Deserialize(new StringReader(xml));
+        } 
+        public static string WriteXml(UserOptions options) {
+            var stream = new MemoryStream();
+            new XmlSerializer(typeof(UserOptions)).Serialize(stream, options);
+            return Encoding.UTF8.GetString(stream.ToArray());
+        } 
+    }
+}
