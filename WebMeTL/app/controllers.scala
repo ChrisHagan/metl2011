fe75f4b6b770b23b7e9a772cc9762f3b6762fe51
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-11-30 13:22:45 +1100

    Synching webmetl
    Former-commit-id: f8d7237c2c50a7c1be9fa94aeb97b495efd39f2c

---
+++ WebMeTL/app/controllers.scala
@@ -0,0 +1,110 @@
+package controllers
+
+import play._
+import play.mvc._
+import play.libs._
+import org.apache.commons.io._
+import org.apache.commons.compress.archivers._
+import org.apache.commons.compress.archivers.zip._
+import java.io.{File}
+import scala.xml.parsing._
+import scala.collection.JavaConversions._
+import org.json.simple._
+import net.liftweb._
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
+import net.liftweb.json.JsonAST._
+import collection.breakOut;
+
+object Application extends Controller {
+    val width = 200
+    val height = 150
+    val server = "https://deified.adm.monash.edu.au:1188/Structure"
+    val username = "exampleUsername"
+    val password = "examplePassword"
+    val TEMP_FILE = "all.zip"
+    val history = preloadHistory
+    val contentWeight = mapRandom(history, 1000)
+    val authorsPerConversation = mapRandom(history, 20)
+    private def preloadHistory = {
+        val zipFuture = WS.url(server+"/all.zip").authenticate(username,password).get
+        FileUtils.writeByteArrayToFile(new File(TEMP_FILE),IOUtils.toByteArray(zipFuture.getStream))
+        val zipFile = new ZipFile(new File(TEMP_FILE))
+        zipFile.getEntries
+            .map(any => any.asInstanceOf[ZipArchiveEntry])
+            .filter(zae=>zae.getName.endsWith("details.xml"))
+            .map(zae => IOUtils.toString(zipFile.getInputStream(zae)))
+            .map(detail => xml.XML.loadString(detail))
+            .toList
+    }
+    private def mapRandom(history:Seq[xml.NodeSeq], max:Int = 10) = Map(history.map(c => ((c \ "jid").text, (Math.random * max).intValue.toString)):_*)
+    def index = conversations
+    def conversations = {
+        val authorJson = authorSummaries(history).toString()
+        Template(authorJson)
+    }
+    private def authorFrequencies(xs:Seq[xml.NodeSeq]) = {
+        val authors = xs.map(x => (x \ "author").text)
+        authors.groupBy(identity).mapValues(_.length)
+    }
+    private def elem(name:String, children:xml.Node*) = xml.Elem(null, name ,xml.Null,xml.TopScope, children:_*); 
+    private def node(name:String, parent:xml.NodeSeq) = elem(name, xml.Text((parent \ name).text))
+    private def authorSummary(kv:Pair[String,Int], xs:Seq[xml.NodeSeq]) = {
+        val author = kv._1
+        val freq = kv._2
+        val count = elem("conversationCount", xml.Text(freq.toString))
+        val conversationsBody = xs.filter(x=>(x \ "author").text == author).map(
+            node=>{
+                val conversationContentWeight = elem("contentVolume", xml.Text(contentWeight((node \ "jid").text)))
+                val authorCount = elem("authorCount", xml.Text(authorsPerConversation((node \ "jid").text)))
+                val slideCount = elem("slideCount", xml.Text((node \ "slide").length.toString))
+                <listing>
+                    {List("title","jid").map(name=>this.node(name, node)) ::: List(conversationContentWeight, authorCount, slideCount)}
+                </listing>;
+            }).toList
+        val conversations = <conversations>{conversationsBody}</conversations>;
+        elem(author, List(count, conversations):_*)
+    }
+    private def authorSummaries(xs:Seq[xml.NodeSeq]) = {
+        val authorXmlList = authorFrequencies(xs).map(kv=>authorSummary(kv, xs))
+        val authors = 
+        <conversationSummaries>
+            {authorXmlList}
+        </conversationSummaries>;
+        val authorsJson = json.Xml.toJson(authors).transform {
+            case JField("listing", x: JObject) => JField("listing", JArray(x :: Nil))
+            case other => other
+        }
+        pretty(JsonAST.render(authorsJson))
+    }
+    def conversation(jid:Int)={
+        val maybeXml = retrieveDetails(jid).map(renderDetails(_))
+        maybeXml match{
+            case Some(foundXml)=>{
+                val resultXml = foundXml
+                Template(resultXml)
+            }
+            case None=> "Conversation could not be found"
+        }
+    }
+    private def retrieveDetails(id:Int):Option[xml.Node]={
+        val uri = "%s/%s/details.xml".format(server,id)
+        try{
+            println(uri)
+            Some(xml.XML.loadString(WS.url(uri).authenticate(username,password).get.getString))
+        }
+        catch{
+            case e: Exception => None
+        }
+    }
+    private def renderDetails(detail:xml.Node):xml.Node={
+        val slides = (detail \\ "slide").map(slide =>{
+            val thumbUri = "http://spacecaps.adm.monash.edu.au:8080/thumb/%s?width=%d&height=%d".format((slide \\ "id").text, width, height)
+            <img src={thumbUri}></img>;
+        })
+        <div>
+            <div>{(detail \\ "title").text}</div>
+            {slides}
+        </div>;
+    }
+}
---
+++ WebMeTL/public/javascripts/addAuthorGraph.js
@@ -0,0 +1,213 @@
+var frequenciesByAuthor = _.reduce(detailedAuthors.conversationSummaries, 
+    function(acc, v, k){
+        acc[k] = v.conversationCount;
+        return acc;
+    }, {});
+var detailsByAuthor = _.reduce(detailedAuthors.conversationSummaries,
+    function(acc,v,k){
+        acc[k] = _.reduce(v.conversations.listing, function(acc, v, k){
+            acc[v.title] = v
+            return acc;
+        }, {})
+        return acc;
+    }, {});
+var flatNodes = pv.dom(frequenciesByAuthor).nodes();
+var clusteredFrequencies = cluster(frequenciesByAuthor,6);
+var clusteredNodes = pv.dom(clusteredFrequencies).nodes();
+var dotScale = pv.Scale.linear(_.min(_.values(frequenciesByAuthor)), _.max(_.values(frequenciesByAuthor))).range(20,1000)
+var marginScale = pv.Scale.linear().range(-100,100)
+
+function deepen(level,k,v){
+    var obj = {}
+    obj[k] = v
+    return _.reduce(_.range(level),function(acc,i){
+        var wrapper = {}
+        wrapper[k] = acc
+        return wrapper 
+    },obj) 
+}
+function cluster(subject, maxClusters){
+    var subjectValues = _.values(subject)
+    var limit = _.max(subjectValues)
+    var divisor = Math.round(limit / maxClusters);
+    return _.reduce(subject, function(acc, v, k){
+        var group = Math.min(Math.floor(v / divisor) + 2, maxClusters)
+        var deepGroup = deepen(group,k,v)
+        return _.extend(acc, deepGroup)
+    }, {});
+}
+
+var Master = {
+    panel:function(){
+        return new pv.Panel()
+            .canvas("fig")
+            .left(0)
+            .top(0)
+            .bottom(0)
+            .right(0)
+    }
+}
+var Authors = {
+    colors:_.reduce(_.keys(detailedAuthors.conversationSummaries), function(acc,item,index){
+        var availableColors = pv.Colors.category19().range();
+        acc[item] = availableColors[index % availableColors.length]
+        return acc;
+    },{}),
+    color:function(d){
+        if(!d.nodeName) return "black"
+        return Authors.colors[d.nodeName]
+    },
+    data:clusteredNodes,
+    squares : function(){
+        var graphRoot = Master.panel()
+        var treemap = graphRoot.add(pv.Layout.Treemap)
+        .def("active", function(){ return -1})
+        .nodes(this.data);
+        treemap.leaf.add(pv.Panel)
+            .fillStyle(Authors.color)
+            .strokeStyle("#fff")
+            .lineWidth(1)
+            .event("click",detail)
+        treemap.label.add(pv.Label);
+        graphRoot.render();
+    },
+    wedges: function(){
+        var graphRoot = Master.panel()
+        var newChild = graphRoot.add(pv.Layout.Partition.Fill)
+            .nodes(Authors.data)
+            .size(function(d){
+                return dotScale(d.nodeValue)})
+            .order("descending")
+            .orient("radial");
+        newChild.node.add(pv.Wedge)
+            .fillStyle(Authors.color)
+            .strokeStyle("#fff")
+            .lineWidth(1)
+        newChild.label.add(pv.Label);
+        graphRoot.render();
+    },
+    radial: function(){
+        var graphRoot = Master.panel()
+        var newChild = graphRoot.add(pv.Layout.Tree)
+            .nodes(Authors.data)
+            .orient("radial")
+        newChild.link.add(pv.Line)
+        newChild.node.add(pv.Dot)
+            .fillStyle(Authors.color)
+            .size(function(d){
+                return dotScale(d.nodeValue)})
+            .event("click",Conversations.radial)
+        newChild.label.add(pv.Label)
+            .visible(function(d){ return d.lastChild == null })
+        graphRoot.render()
+    },
+    force:function(){
+        var graphRoot = Master.panel();
+        var nodes = []
+        var links = []
+        var ptr = 0
+        _.each(frequenciesByAuthor, function(f,a){
+            nodes.push({freq:f,author:a})
+            links.push({source:ptr++,target:0,value:25})
+        })
+        var newChild = graphRoot.add(pv.Layout.Force)
+            .nodes(nodes)
+            .links(links)
+        newChild.link.add(pv.Line)
+        newChild.node.add(pv.Dot)
+            .size(function(d){return dotScale(d.freq)})
+            .fillStyle("red")
+            .anchor("center")
+            .add(pv.Label)
+            .text(function(d){return d.author})
+        graphRoot.render()
+    }
+}
+var Conversations = {
+    color:pv.Scale.linear(0,10).range("white","red"),
+    radial:function(node){
+        var graphRoot = Master.panel()
+        var author = node.nodeName;
+        var data = detailsByAuthor[author]
+        var nodes = pv.dom(data)
+            .leaf(function(d){
+                return "jid" in d
+            })
+            .nodes()
+        var newChild = graphRoot.add(pv.Layout.Tree)
+            .nodes(nodes)
+            .orient("radial")
+            .depth(300)
+            .breadth(20)
+        newChild.link.add(pv.Line)
+        newChild.node.add(pv.Dot)
+            .fillStyle(function(d){
+                if(!d.nodeValue) return "white"
+                return Conversations.color(d.nodeValue.authorCount)})
+            .size(function(d){
+                if(!d.nodeValue) return 0
+                return d.nodeValue.contentVolume})
+            .event("click",Conversation.slides)
+        newChild.label.add(pv.Label)
+            .text(function(d){
+                if(!d.nodeValue) return ""
+                return d.nodeValue.title})
+        graphRoot.render()
+        Breadcrumb.add(author, function(){Conversations.radial(node)})
+    }
+}
+var Conversation = {
+    slides:function(node){
+        var width = 1024
+        var height = 768
+        var conversation = node.nodeValue
+        var jid = parseInt(conversation.jid)
+        var slideCount = parseInt(conversation.slideCount)
+        var nodes = pv.range(jid+1, jid+slideCount)
+        var graphRoot = new pv.Panel()
+            .canvas("slideDisplay")
+            .width(width*2) 
+            .height(slideCount * height)
+        graphRoot.add(pv.Image)
+            .data(nodes)
+            .top(function(){return this.index * height})
+            .width(width)
+            .height(height)
+            .url(function(i){
+                return "http://localhost:8080?width="+width+"&height="+height+"&server=deified&slide="+i
+            })
+            .strokeStyle("black")
+            .anchor("right")
+            .add(pv.Panel)
+                .width(width)
+                .height(height)
+                .top(function(d){return d.y - height /2})
+                .add(pv.Rule)
+        graphRoot.render()
+        Breadcrumb.add(conversation.title, function(){Conversation.slides(node)})
+    }
+}
+var Breadcrumb = (function(){ 
+    var trail = []
+    var container = $("#breadcrumb")          
+    return {
+        add: function(label, func){
+            if(_.contains(_.pluck(trail, "label"), label)) return;
+            trail.push({label:label, func:func})
+            this.render()
+        },
+        render:function(){
+            container.html("")
+            var recentCrumbs = [{label:"All authors", func:Authors.radial}]
+            _.each(trail.slice(-10), function(crumb){recentCrumbs.push(crumb)})
+            _.each(recentCrumbs, function(crumb){
+                container.append($("<span />").append(crumb.label.slice(0,15) + "->").click(function(){
+                    scroll(0,0)
+                    crumb.func()
+                }))
+            })
+        }
+    }
+})()
+Authors.radial()
+Breadcrumb.render()
