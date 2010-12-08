aa27a12364c66ea361704721603369acc0af6cf2
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-12-09 01:13:06 +1100

    Added new unix compatible thumbservice to webmetl
    Former-commit-id: 938749c5ed3787fcaa0e2caf8fefde5b1f0da9ba

--- WebMeTL/app/controllers.scala
+++ WebMeTL/app/controllers.scala
@@ -16,10 +16,12 @@ import net.liftweb.json.JsonDSL._
 import net.liftweb.json.JsonAST._
 import collection.breakOut;
 import MeTL._
+import java.awt.image._
 
 object Application extends Controller {
     val width = 200
     val height = 150
+    val server = "https://deified.adm.monash.edu.au:1188"
     val structure = "https://deified.adm.monash.edu.au:1188/Structure"
     val history = "https://deified.adm.monash.edu.au:1749/"
     val username = "exampleUsername"
@@ -59,7 +61,7 @@ object Application extends Controller {
         val clump = MessageReductor.clump(relativizedMessages, true)
         pretty(render(clump.toJsonFull))
     }
-    private def slide(jid:Int)={
+    private def slideXmppMessages(jid:Int)={
         val zipFuture = WS.url("%s/%s/all.zip".format(history,jid)).authenticate(username,password).get
         FileUtils.writeByteArrayToFile(new File(TEMP_FILE),IOUtils.toByteArray(zipFuture.getStream))
         val zipFile = new ZipFile(new File(TEMP_FILE))
@@ -68,6 +70,9 @@ object Application extends Controller {
             .filter(zae=>zae.getName.endsWith(".xml"))
             .map(zae => IOUtils.toString(zipFile.getInputStream(zae))+"</logCollection>")
             .map(detail => xml.XML.loadString(detail))
+    }
+    private def slide(jid:Int)={
+        slideXmppMessages(jid)
             .foldLeft(List.empty[Seq[Message]])((acc,item)=>{
                 (item \\ "message").flatMap(message=>{
                     val timestamp = (message \ "@time").text.toLong
@@ -79,7 +84,44 @@ object Application extends Controller {
                            })
                         })
                 }) :: acc
-            }).flatten
+        }).flatten
+    }
+    def snapshot(jid:Int, width:Int=640, height:Int=320)={
+        import java.awt._
+        request.contentType = "image/png"
+        val image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB)
+        val g = image.createGraphics.asInstanceOf[java.awt.Graphics2D]
+        g.setPaint(Color.white)
+        g.fill(new Rectangle(0,0,width,height))
+        for(m <- slideXmppMessages(jid)){
+            for(s <- (m \\ "image")){
+                val source = (s \ "source").text
+                val width = (s \ "width").text.toFloat.toInt
+                val height = (s \ "height").text.toFloat.toInt
+                val x = (s \ "x").text.toFloat.toInt
+                val y = (s \ "y").text.toFloat.toInt
+                val img = javax.imageio.ImageIO.read(WS.url(server+source).authenticate(username,password).get.getStream)
+                g.drawImage(img,x,y,null)
+            }
+            for(s <- (m \\ "ink")){
+                (s \ "color").text.split(" ").map(_.toInt) match{
+                    case Array(red,green,blue,alpha) => g.setPaint(new java.awt.Color(red,green,blue,alpha))
+                }
+                g.setStroke(new BasicStroke((s \ "thickness").text.toInt))
+                for(ps <- (s \ "points").text.split(" ").map(_.toFloat.toInt).grouped(3).grouped(2)){
+                    ps match{
+                        case Seq(Array(x1,y1,_),Array(x2,y2,_))=>{
+                            println(x1,y1,x2,y2)
+                            g.drawLine(x1,y1,x2,y2)
+                        }
+                        case Seq(_)=>false//We lose the last point if odd 
+                    }
+                }
+            }
+        }
+        val baos = new java.io.ByteArrayOutputStream()
+        javax.imageio.ImageIO.write(image, "png", baos)
+        new java.io.ByteArrayInputStream(baos.toByteArray())
     }
     private def authorFrequencies(xs:Map[Int,xml.NodeSeq]) = {
         val authors = xs.map(kv => (kv._2 \ "author").text)
