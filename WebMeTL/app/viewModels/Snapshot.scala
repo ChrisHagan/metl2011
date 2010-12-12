a28fc6469615a68aa8ed6934b11bd7f68fd55eb7
Chris <Chris Hagan captain.mister.hagan@gmail.com>
2010-12-12 22:44:30 +1100

    Moved to bicubic rendering
    Former-commit-id: 3cd0e142e659eaaba902506adb2269663bd7cfad

--- WebMeTL/app/viewModels/Snapshot.scala
+++ WebMeTL/app/viewModels/Snapshot.scala
@@ -22,29 +22,23 @@ object Snapshot{
             g.drawImage(img,x,y,width,height,null)
         }
     }
-    case class HistoricalInk(identity:String,color:String,thickness:Float,points:immutable.List[Array[Int]])extends HistoricalItem{
+    case class HistoricalInk(identity:String,color:String,thickness:Float,points:immutable.List[Array[Float]])extends HistoricalItem{
         override def render(g:java.awt.Graphics2D) ={
             color.split(" ").map(_.toInt) match{
                 case Array(red,green,blue,alpha) => g.setPaint(new java.awt.Color(red,green,blue,alpha))
             }
-            points.sliding(2).foreach(pts=>
-                pts.length match{
-                    case 2 =>{
-                        //val pressure = 0.75 - (((pts(0)(2)+pts(1)(2)) / 2.0) / 255.0)
-                        val pressure = 0.1
-                        g.setStroke(new java.awt.BasicStroke(thickness+pressure.toFloat))
-                        g.draw(new java.awt.geom.Line2D.Double(pts(0)(0),pts(0)(1),pts(1)(0),pts(1)(1)))
-                    }
-                    case _ => false
-                })
+            val pressure = 0.22
+            g.setStroke(new java.awt.BasicStroke(thickness+pressure.toFloat))
+            val vectorizer = new BasicVectorizer
+            points.foreach(pts=>vectorizer.add(pts(0),pts(1),0))
+            g.draw(vectorizer.getShape)
         }
     }
     val resourceCache = collection.mutable.Map.empty[String,java.awt.Image]
     def png(width:Int,height:Int,messages:immutable.List[String]):BufferedImage = {    
-        val preTime = Stopwatch.start
+        println
         var preParser = mutable.ListBuffer.empty[HistoricalItem]
         val relevantNodes = Array("image", "ink", "dirtyInk", "dirtyImage")
-        preTime("Got messages")
         val time = Stopwatch.start
         val nodes = 
             messages.map( message=>
@@ -55,7 +49,7 @@ object Snapshot{
             .toList.flatten
         time("Filtered nodes")
         val maxs = nodes.map(s=>{
-            val farPoints = mutable.ListBuffer.empty[Array[Int]]
+            val farPoints = mutable.ListBuffer.empty[Array[Float]]
             s.label match{
                 case "image"=> {
                     val source = (s \ "source").text
@@ -72,7 +66,7 @@ object Snapshot{
                     val color = (s \ "color").text                
                     val thickness = (s \ "thickness").text.toFloat
                     val pointText = (s \ "points").text
-                    val points = pointText.split(" ").map(sPt=>sPt.toDouble.toInt).grouped(3).toList
+                    val points = pointText.split(" ").map(sPt=>sPt.toFloat).grouped(3).toList
                     points.foreach(p=> farPoints += p)
                     preParser += HistoricalInk(identity,color,thickness.toInt,points)
                 }
@@ -85,16 +79,16 @@ object Snapshot{
         })
         time("Parsed and measured")
         val flatmaxs = maxs.flatten
-        val maxX = flatmaxs.length match{
+        val maxX = (flatmaxs.length match{
             case 0 => width
             case _ => flatmaxs.map(p=>p(0)).max
-        }
-        val maxY = flatmaxs.length match{
+        }).toInt
+        val maxY = (flatmaxs.length match{
             case 0 => height
             case _ => flatmaxs.map(p=>p(1)).max
-        }
+        }).toInt
         time("Max dimensions calculated",maxX,maxY)
-        val unscaledImage = new BufferedImage(maxX,maxY,BufferedImage.TYPE_INT_RGB)
+        val unscaledImage = new BufferedImage(maxX,maxY,BufferedImage.TYPE_3BYTE_BGR)
         val g = unscaledImage.createGraphics.asInstanceOf[Graphics2D]
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setPaint(Color.white)
@@ -102,8 +96,9 @@ object Snapshot{
         preParser.filter(_.isInstanceOf[HistoricalImage]).foreach(_.render(g))
         preParser.filter(_.isInstanceOf[HistoricalInk]).foreach(_.render(g))
         time("Finished painting unscaled") 
-        val scaledImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB)
+        val scaledImage = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR)
         val scaledG = scaledImage.createGraphics.asInstanceOf[Graphics2D]
+        scaledG.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         scaledG.setPaint(Color.white)
         scaledG.fill(new Rectangle(0,0,width,height))
         val xScale = width.toDouble / maxX.toDouble
