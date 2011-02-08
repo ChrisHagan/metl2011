4fc6fc8810e03930596209bfef882ce83ac3bd34
stu <devnull@localhost>
2011-02-08 11:48:57 +1100

    clickonce 87 rolled, web address moved from metlpresenter to MeTL2011
    Former-commit-id: 5da4a5c8a80bcd2e9783dec5e32d6bf669edab2a

--- MeTLMeeting/DeployLectureMeTLClickonce.bat
+++ MeTLMeeting/DeployLectureMeTLClickonce.bat
@@ -4,10 +4,10 @@ echo
 echo (make sure that when you published, you changed:
 echo SandRibbon - Properties - Publish -
 echo "Installation Folder URL"
-echo to: http://metl.adm.monash.edu/MeTLPresenter/
+echo to: http://metl.adm.monash.edu/MeTL2011/
 echo Updates -
 echo "Update Location"
-echo to: http://metl.adm.monash.edu/MeTLPresenter/
+echo to: http://metl.adm.monash.edu/MeTL2011/
 echo Options -
 echo Product name:
 echo MeTL Presenter
@@ -18,8 +18,8 @@ echo Don't use this command unless you're ready to overwrite
 echo the currently deployed Clickonce on the server (drawkward.adm) 
 echo Press Ctrl+C to exit this.  Any other keypress will continue
 pause
-pscp.exe -r -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\Application Files" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTLPresenter/
-pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\index.html" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTLPresenter/
-pscp.exe -r -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\office2007piaredist" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTLPresenter/
-pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\MeTL Presenter.application" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTLPresenter/
-pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\setup.exe" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTLPresenter/
+pscp.exe -r -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\Application Files" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTL2011/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\index.html" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTL2011/
+pscp.exe -r -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\office2007piaredist" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTL2011/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\MeTL Presenter.application" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/2011/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\setup.exe" deploy@reviver.adm.monash.edu.au:/srv/racecarDeploy/MeTL2011/
