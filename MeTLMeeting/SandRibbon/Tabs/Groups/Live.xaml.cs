ebab8d874159e05583777dac03f32e5cec1fd96b
chris <devnull@localhost>
2011-03-04 12:38:42 +1100

    Added tab
    Former-commit-id: a93197f7a212ccfb05e6188bcce966d6c73a5f36

---
+++ MeTLMeeting/SandRibbon/Tabs/Groups/Live.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+using SandRibbon.Providers;
+using MeTLLib.DataTypes;
+
+namespace SandRibbon.Tabs.Groups
+{
+    public partial class Live
+    {
+        public Live()
+        {
+            InitializeComponent();
+        }
+        private void Participate_Click(object sender, RoutedEventArgs e)
+        {
+            if(Globals.location.currentSlide != 0)
+                Commands.ListenToAudio.Execute(Slide.ConversationFor(Globals.location.currentSlide));
+        }
+    }
+}
