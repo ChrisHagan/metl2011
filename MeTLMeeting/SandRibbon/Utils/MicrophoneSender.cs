e32a975e9bca75373d4fe1fe4d4bf9dd9a0891e0
David <David Hagan (david.hagan@monash.edu)>
2011-03-08 17:44:41 +1100

    cleaned up the structure a little.  Now just needs MP3 encoding
    Former-commit-id: 64091259e2f834bf4b27c658e9d92a02416784ff

--- MeTLMeeting/SandRibbon/Utils/MicrophoneSender.cs
+++ MeTLMeeting/SandRibbon/Utils/MicrophoneSender.cs
@@ -16,6 +16,11 @@ namespace SandRibbon.Utils
     {
         private static Socket socket;
         private static WaveIn microphoneDevice;
+        private static readonly string MP3Name = "MeTL Lecturer";
+        private static readonly string MP3Genre = "Unicast Lecture Content";
+        private static readonly string MP3Description = "This is a live stream of whatever";
+        private static readonly int MP3Bitrate = 160;
+        private static readonly string MP3Url = "http://metl.adm.monash.edu.au/MeTL2011";
         public static void startSending()
         {
             if (microphoneDevice == null)
@@ -50,10 +55,9 @@ namespace SandRibbon.Utils
             socket.ReceiveTimeout = 20000;
             socket.Send(Encoding.UTF8.GetBytes(headers));
             socket.Receive(responseBuffer);
-
             if (Encoding.UTF8.GetString(responseBuffer) == "OK2\r\nicy-caps:11\r\n\r\n")
             {
-                socket.Send(Encoding.UTF8.GetBytes("icy-name:Unnamed Server\r\nicy-genre:Unknown Genre\r\nicy-pub:1\r\nicy-br:160\r\nicy-url:http://www.shoutcast.com\r\nicy-irc:%23shoutcast\r\nicy-icq:0\r\nicy-aim:N%2FA\r\n\r\n"));
+                socket.Send(Encoding.UTF8.GetBytes(string.Format("icy-name:{0}\r\nicy-description:{4}\r\nicy-genre:{1}\r\nicy-pub:0\r\nicy-br:{2}\r\nicy-url:{3}\r\nicy-irc:%23shoutcast\r\nicy-icq:0\r\nicy-aim:N%2FA\r\n\r\n",MP3Name,MP3Genre,MP3Bitrate,MP3Url,MP3Description)));
                 return true;
             }
             else return false;
@@ -65,6 +69,7 @@ namespace SandRibbon.Utils
         }
         private static byte[] transcode(byte[] buffer)
         {
+            //so, here the wav-encoded data needs to be transcoded into 160kbps Mp3.  In fact, we can drop considerably from 160, but I figure, why not start high?
             return buffer;
         }
     }
