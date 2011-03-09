d449d9f42238576a39fc53956cd0653f218e4b5d
David <David Hagan (david.hagan@monash.edu)>
2011-03-09 12:56:12 +1100

    mp3 conversion is now working
    Former-commit-id: 75d9e778b73796dfc9df28576b4b70dea061cd90

--- MeTLMeeting/SandRibbon/Utils/MicrophoneSender.cs
+++ MeTLMeeting/SandRibbon/Utils/MicrophoneSender.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Windows;
 using SandRibbon.Utils.Microphone;
 using System.Net.Sockets;
+using Yeti.Lame;
 
 namespace SandRibbon.Utils
 {
@@ -41,7 +42,7 @@ namespace SandRibbon.Utils
         }
         private static void establishMicrophone()
         {
-            microphoneDevice = new WaveIn(WaveIn.Devices.Where(s => s.Name.Contains("Mic")).FirstOrDefault(), 44100, 16, 1, 98200);
+            microphoneDevice = new WaveIn(WaveIn.Devices.Where(s => s.Name.Contains("Mic")).FirstOrDefault(), 44100, 16, 2, 98200);
             microphoneDevice.BufferFull += sendBuffer;
         }
         private static bool sendShoutcastHeaders()
@@ -57,20 +58,33 @@ namespace SandRibbon.Utils
             socket.Receive(responseBuffer);
             if (Encoding.UTF8.GetString(responseBuffer) == "OK2\r\nicy-caps:11\r\n\r\n")
             {
-                socket.Send(Encoding.UTF8.GetBytes(string.Format("icy-name:{0}\r\nicy-description:{4}\r\nicy-genre:{1}\r\nicy-pub:0\r\nicy-br:{2}\r\nicy-url:{3}\r\nicy-irc:%23shoutcast\r\nicy-icq:0\r\nicy-aim:N%2FA\r\n\r\n",MP3Name,MP3Genre,MP3Bitrate,MP3Url,MP3Description)));
+                socket.Send(Encoding.UTF8.GetBytes(string.Format("icy-name:{0}\r\nicy-description:{4}\r\nicy-genre:{1}\r\nicy-pub:0\r\nicy-br:{2}\r\nicy-url:{3}\r\nicy-irc:%23shoutcast\r\nicy-icq:0\r\nicy-aim:N%2FA\r\n\r\n", MP3Name, MP3Genre, MP3Bitrate, MP3Url, MP3Description)));
                 return true;
             }
             else return false;
         }
         private static void sendBuffer(byte[] buffer)
         {
+            if (socket == null || !socket.Connected)
+                sendShoutcastHeaders();
             if (socket != null && socket.Connected)
                 socket.Send(transcode(buffer));
         }
         private static byte[] transcode(byte[] buffer)
         {
-            //so, here the wav-encoded data needs to be transcoded into 160kbps Mp3.  In fact, we can drop considerably from 160, but I figure, why not start high?
-            return buffer;
+            MemoryStream outputStream = new MemoryStream();
+            try
+            {
+                var writer = new Yeti.MMedia.Mp3.Mp3Writer(outputStream, new WaveLib.WaveFormat(44100, 16, 2));
+                writer.WriterConfig.Format.cbSize = 160;
+                writer.Write(buffer);
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in Mp3 Conversion :" + e.Message);
+            }
+            return outputStream.ToArray();
         }
     }
 }
