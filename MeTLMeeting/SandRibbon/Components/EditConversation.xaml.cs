6ac50af5f4ce112a251ff9ccec8ccf3d6373c629
stu <devnull@localhost>
2011-03-25 14:18:38 +1100

    cleaned drag code
    Former-commit-id: f525c54cbf458b5362898573f09bc78a03d9cf66

--- MeTLMeeting/SandRibbon/Components/EditConversation.xaml.cs
+++ MeTLMeeting/SandRibbon/Components/EditConversation.xaml.cs
@@ -17,7 +17,6 @@ using MeTLLib;
 using MeTLLib.DataTypes;
 using Microsoft.Practices.Composite.Presentation.Commands;
 using SandRibbon.Providers;
-using SimplestDragDrop;
 
 namespace SandRibbon.Components
 {
@@ -25,45 +24,16 @@ namespace SandRibbon.Components
     /// Interaction logic for EditConversation.xaml
     /// </summary>
     /// 
-    public class ReorderSlideToThumbConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            var source = (System.Windows.Controls.Image)values[0];
-            var id = ((ReorderSlide)source.DataContext).slide.id;
-            ThumbnailProvider.thumbnail(source, id);
-            return null;
-        }
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class ReorderSlideIndexConverter : IValueConverter
-    {
-        private ObservableCollection<ReorderSlide> collection;
-        public ReorderSlideIndexConverter(ObservableCollection<ReorderSlide> collection)
-        {
-            this.collection = collection;
-        }
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return collection.IndexOf((ReorderSlide)value) + 1;
-        }
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return value;
-        }
-    }
+    
     public partial class EditConversation : Window
     {
-        public ObservableCollection<ReorderSlide> activeSlideList = new ObservableCollection<ReorderSlide>();
-        public static ReorderSlideToThumbConverter SlideToThumb;
-        public static ReorderSlideIndexConverter SlideIndex;
+        public ObservableCollection<Slide> activeSlideList = new ObservableCollection<Slide>();
+        public static SlideToThumbConverter SlideToThumb;
+        public static SlideIndexConverter SlideIndex;
         public EditConversation()
         {
-            SlideToThumb = new ReorderSlideToThumbConverter();
-            SlideIndex = new ReorderSlideIndexConverter(activeSlideList);
+            SlideToThumb = new SlideToThumbConverter();
+            SlideIndex = new SlideIndexConverter(activeSlideList);
             InitializeComponent();
             activeSlides.ItemsSource = activeSlideList;
             loadConversation(Globals.conversationDetails.Slides);
@@ -74,7 +44,7 @@ namespace SandRibbon.Components
             foreach (var slide in slides.OrderBy(s => s.index))
             {
                 var image = new Image();
-                activeSlideList.Add(new ReorderSlide(image, slide));
+                activeSlideList.Add(slide);
                 ThumbnailProvider.thumbnail(image, slide.id);
 
             }
@@ -83,7 +53,7 @@ namespace SandRibbon.Components
         {
             var details = Globals.conversationDetails;
             foreach (var slide in activeSlideList)
-                details.Slides.Where(s => s.id == slide.slide.id).First().index = activeSlideList.IndexOf(slide);
+                details.Slides.Where(s => s.id == slide.id).First().index = activeSlideList.IndexOf(slide);
             ClientFactory.Connection().UpdateConversationDetails(details);
             Close();
         }
@@ -91,9 +61,10 @@ namespace SandRibbon.Components
         {
             if (activeSlides.SelectedItem != null)
             {
-                var item = ((ReorderSlide)activeSlides.SelectedItem);
+                var item = ((Slide)activeSlides.SelectedItem);
                 activeSlideList.Remove(item);
                 activeSlideList.Insert(0, item);
+                activeSlides.ScrollIntoView(item);
             }
 
         }
@@ -101,22 +72,11 @@ namespace SandRibbon.Components
         {
             if (activeSlides.SelectedItem != null)
             {
-                var item = ((ReorderSlide)activeSlides.SelectedItem);
+                var item = ((Slide)activeSlides.SelectedItem);
                 activeSlideList.Remove(item);
                 activeSlideList.Add(item);
+                activeSlides.ScrollIntoView(item);
             }
         }
     }
-
-	public class ReorderSlide
-	{
-		public Image Location { get; set; }
-        public Slide slide { get; set; }
-
-		public ReorderSlide(Image location, Slide slide)
-		{
-			this.Location = location;
-            this.slide = slide;
-		}
-	}
 }
