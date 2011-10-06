52d649b460bdfafcb2f1ec63963948a8c89a87bd
josh <joshua.jordan@monash.edu>
2011-10-06 12:09:40 +1100

    Added generated unit tests for UITestFramework
    Former-commit-id: 2342f5715c5cbcc415d1c2cee1e02e31a0b6f3df

---
+++ MeTLMeeting/Functional/UITestHelperTest.cs
@@ -0,0 +1,141 @@
+﻿using UITestFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Automation;
+
+namespace Functional
+{
+    
+    
+    /// <summary>
+    ///This is a test class for UITestHelperTest and is intended
+    ///to contain all UITestHelperTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class UITestHelperTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for UITestHelper Constructor
+        ///</summary>
+        [TestMethod()]
+        public void UITestHelperConstructorTest()
+        {
+            UITestHelper target = new UITestHelper();
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for FindFirstChildUsingAutomationId
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("UITestFramework.dll")]
+        public void FindFirstChildUsingAutomationIdTest()
+        {
+            UITestHelper_Accessor target = new UITestHelper_Accessor(); // TODO: Initialize to an appropriate value
+            string controlAutomationId = string.Empty; // TODO: Initialize to an appropriate value
+            AutomationElement expected = null; // TODO: Initialize to an appropriate value
+            AutomationElement actual;
+            actual = target.FindFirstChildUsingAutomationId(controlAutomationId);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for WaitForControl
+        ///</summary>
+        [TestMethod()]
+        public void WaitForControlTest()
+        {
+            UITestHelper target = new UITestHelper(); // TODO: Initialize to an appropriate value
+            string controlAutomationId = string.Empty; // TODO: Initialize to an appropriate value
+            UITestHelper.Condition loopCondition = null; // TODO: Initialize to an appropriate value
+            UITestHelper.Condition returnCondition = null; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.WaitForControl(controlAutomationId, loopCondition, returnCondition);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for WaitForControlEnabled
+        ///</summary>
+        [TestMethod()]
+        public void WaitForControlEnabledTest()
+        {
+            UITestHelper target = new UITestHelper(); // TODO: Initialize to an appropriate value
+            string controlAutomationId = string.Empty; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.WaitForControlEnabled(controlAutomationId);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for WaitForControlNotExist
+        ///</summary>
+        [TestMethod()]
+        public void WaitForControlNotExistTest()
+        {
+            UITestHelper target = new UITestHelper(); // TODO: Initialize to an appropriate value
+            string controlAutomationId = string.Empty; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.WaitForControlNotExist(controlAutomationId);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
