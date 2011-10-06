f5b90875cf255fba4781428cc5f19c14a4ace98a
josh <joshua.jordan@monash.edu>
2011-10-06 17:15:32 +1100

    Added CloseInstance test
    Former-commit-id: bc68a2cd1ce3e7b4c79640f3aef57cbb03b81ee9

--- MeTLMeeting/Functional/ShutdownTests.cs
+++ MeTLMeeting/Functional/ShutdownTests.cs
@@ -25,5 +25,23 @@ namespace Functional
             
             Assert.IsTrue(success, ErrorMessages.PROBLEM_SHUTTING_DOWN);
         }
+
+        [TestMethod]
+        public void CloseInstance()
+        {
+            // TODO: Test that there is only one instance
+            if (metlWindows == null)
+                metlWindows = MeTL.GetAllMainWindows();
+
+            foreach (AutomationElement window in metlWindows)
+            {
+                new ApplicationPopup(window).Quit();
+            }
+
+            var control = new UITestHelper();
+            var success = control.WaitForControlNotExist(Constants.ID_METL_MAIN_WINDOW);
+            
+            Assert.IsTrue(success, ErrorMessages.PROBLEM_SHUTTING_DOWN);
+        }
     }
 }
