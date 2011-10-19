2f823d1e4bee506cc60573a74d858954749a99fa
josh <joshua.jordan@monash.edu>
2011-10-20 10:05:58 +1100

    Removed menu items as requested, added missing files
    Former-commit-id: c39f7a5e7ea401f47ae08f5c6e873484bdfeb992

---
+++ MeTLMeeting/Functional/CheckVersionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UITestFramework;
+using System.Windows.Automation;
+
+namespace Functional
+{
+    [TestClass]
+    public class CheckVersionTests
+    {
+        private UITestHelper metlWindow;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            metlWindow = new UITestHelper();
+            metlWindow.SearchProperties.Add(new PropertyExpression(AutomationElement.AutomationIdProperty, Constants.ID_METL_MAIN_WINDOW));
+
+            var success = metlWindow.WaitForControlEnabled();
+            Assert.IsTrue(success, ErrorMessages.EXPECTED_MAIN_WINDOW);
+        }
+
+        [TestMethod]
+        public void CheckVersionAgainstHardcoded()
+        {
+        }
+    }
+}
