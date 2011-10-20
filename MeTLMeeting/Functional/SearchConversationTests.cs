b8eaf7dd23406ee04c0d750793863fb605a7a6e0
josh <joshua.jordan@monash.edu>
2011-10-20 14:23:07 +1100

    Health check: Search and Join owned and unowned conversations
    Former-commit-id: 8fe52295c99d01ca0fcf9589a9d177194964a4f1

---
+++ MeTLMeeting/Functional/SearchConversationTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UITestFramework;
+using System.Windows.Automation;
+
+namespace Functional
+{
+    [TestClass]
+    public class SearchConversationTests
+    {
+        private UITestHelper metlWindow;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            metlWindow = new UITestHelper();
+            metlWindow.SearchProperties.Add(new PropertyExpression(AutomationElement.AutomationIdProperty, Constants.ID_METL_MAIN_WINDOW));
+
+            var success = metlWindow.WaitForControlExist();
+            Assert.IsTrue(success, ErrorMessages.EXPECTED_MAIN_WINDOW);
+        }
+        
+        [TestMethod]
+        public void SearchForOwnedAndJoin()
+        {
+            var search = new ConversationSearcher(metlWindow.AutomationElement);
+
+            search.searchField("TEST_FindAndJoinConversationOwned");
+            search.Search();
+
+            search.JoinFirstFound();
+        }
+
+        [TestMethod]
+        public void SearchForHavePermissionAndJoin()
+        {
+            var search = new ConversationSearcher(metlWindow.AutomationElement);
+
+            search.searchField("TEST_FindAndJoinConversationNotOwned");
+            search.Search();
+
+            search.JoinFirstFound();
+        }
+    }
+}
