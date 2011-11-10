866e90b6c8754f14ec9493c58c28581baad43f15
josh <devnull@localhost>
2011-11-11 09:42:32 +1100

    SandRibbon proj changed for Staging rollout, this needs to be changed for a production roll
    Former-commit-id: ead2ec2d127c847f3ccd8d0d13071809d53bc0df

--- MeTLMeeting/SandRibbon/Properties/AssemblyInfo.cs
+++ MeTLMeeting/SandRibbon/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SandRibbon")]
-[assembly: AssemblyDescription("SandRibbon")]
+[assembly: AssemblyTitle("MeTL Staging")]
+[assembly: AssemblyDescription("MeTL Staging")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SandRibbon")]
+[assembly: AssemblyProduct("MeTL Staging")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
