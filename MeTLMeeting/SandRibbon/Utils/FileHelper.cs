bbaa33fca6da8bb7f80a0100bfebb35ddfdf42fa
josh <joshua.jordan@monash.edu>
2011-12-09 12:35:26 +1100

    Added missing files
    Former-commit-id: ead93a07296f253da15df7752d3d97728e463e01

---
+++ MeTLMeeting/SandRibbon/Utils/FileHelper.cs
@@ -0,0 +1,150 @@
+﻿using System.IO;
+using System;
+
+namespace SandRibbon.Utils
+{
+    public class FileHelper
+    {
+        private const int KILOBYTE = 1024;
+        private const int MEGABYTE = 1024 * KILOBYTE;
+
+        public static long BytesToMegabytes(long size)
+        {
+            if (size < 0) throw new ArgumentOutOfRangeException("size must be positive number");
+
+            return size / MEGABYTE; 
+        }
+        /// <summary>
+        /// Returns true if the filename specified has a size less than sizeInMegabytes
+        /// </summary>
+        /// <remarks>
+        /// Local is determined by whether the filename starts with http
+        /// If the filename is not local, then return true, ie don't care what is the size of the file 
+        /// </remarks>
+        /// <param name="filename">Locally accessibly filename</param>
+        /// <param name="sizeInMegabytes">Maximum (exclusive) size of the file</param>
+        /// <returns>True if file is less than sizeInMegabytes, false otherwise</returns>
+        public static bool LocalFileLessThanSizeInMegabytes(string filename, int sizeInMegabytes)
+        {
+            if (filename.StartsWith("http")) return true;
+
+            var info = new FileInfo(filename);
+            return info.Length < (sizeInMegabytes * MEGABYTE); // convert sizeInMegabytes to bytes
+        }
+
+        public static string DetermineFileTypeFromExtension(string url)
+        {
+            var extension = Path.GetExtension(url).ToLower();
+            switch (extension)
+            {
+                case ".ppt":
+                    return "PowerPoint";
+                case ".pptx":
+                    return "PowerPoint";
+                case ".doc":
+                    return "Word";
+                case ".docx":
+                    return "Word";
+                case ".txt":
+                    return "Text";
+                case ".html":
+                    return "HTML";
+                case ".xls":
+                    return "Excel";
+                case ".xlsx":
+                    return "Excel";
+                case ".pdf":
+                    return "PDF";
+                case ".odt":
+                    return "Open Office Document";
+                case ".mpg":
+                    return "Video";
+                case ".mp4":
+                    return "Video";
+                case ".m4v":
+                    return "Video";
+                case ".mpz":
+                    return "Video";
+                case ".mpeg":
+                    return "Video";
+                case ".divx":
+                    return "Video";
+                case ".xvid":
+                    return "Video";
+                case ".avi":
+                    return "Video";
+                case ".mov":
+                    return "QuickTime";
+                case ".swf":
+                    return "Shockwave";
+                case ".wmv":
+                    return "Windows Media Video";
+                case ".xap":
+                    return "Silverlight";
+                case ".gif":
+                    return "GIF";
+                case ".png":
+                    return "PNG";
+                case ".bmp":
+                    return "Bitmap";
+                case ".jpeg":
+                    return "Jpeg";
+                case ".jpg":
+                    return "Jpeg";
+                case ".mp3":
+                    return "Audio";
+                case ".wav":
+                    return "Audio";
+                default :
+                    return "Other";
+            }
+        }
+
+        public static string GetFileTypeImageResource(string url)
+        {
+            switch (DetermineFileTypeFromExtension(url))
+            {
+                case "HTML":
+                    return "\\resources\\mimeTypes\\web.png";
+                case "Jpeg":
+                    return "\\resources\\mimeTypes\\image.png";
+                case "Audio":
+                    return "\\resources\\mimeTypes\\audio.png";
+                case "Other":
+                    return "\\resources\\mimeTypes\\unknown.png";
+                case "Bitmap":
+                    return "\\resources\\mimeTypes\\image.png";
+                case "PDF":
+                    return "\\resources\\mimeTypes\\publication.png";
+                case "Text":
+                    return "\\resources\\mimeTypes\\text.png";
+                case "Word":
+                    return "\\resources\\mimeTypes\\document.png";
+                case "PowerPoint":
+                    return "\\resources\\mimeTypes\\publication.png";
+                case "Excel":
+                    return "\\resources\\mimeTypes\\spreadsheet.png";
+                case "PNG":
+                    return "\\resources\\mimeTypes\\image.png";
+                case "JPG":
+                    return "\\resources\\mimeTypes\\image.png";
+                case "GIF":
+                    return "\\resources\\mimeTypes\\image.png";
+                case "Windows Media Video":
+                    return "\\resources\\mimeTypes\\video.png";
+                case "Open Office Document":
+                    return "\\resources\\mimeTypes\\document.png";
+                case "Silverlight":
+                    return "\\resources\\mimeTypes\\gadget.png";
+                case "Shockwave":
+                    return "\\resources\\mimeTypes\\gadget.png";
+                case "Quicktime":
+                    return "\\resources\\mimeTypes\\video.png";
+                case "Video":
+                    return "\\resources\\mimeTypes\\video.png";
+                default:
+                    return "\\resources\\mimeTypes\\unknown.png";
+            }
+        }
+    }
+}
---
+++ MeTLMeeting/SandRibbon/Utils/OpenFileForUpload.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows;
+using SandRibbon.Components.Utility;
+using System.ComponentModel;
+using MeTLLib.DataTypes;
+using SandRibbon.Providers;
+using System.Windows.Threading;
+
+namespace SandRibbon.Utils
+{
+    public class OpenFileForUpload
+    {
+        private Window _owner;
+
+        public OpenFileForUpload(Window owner)
+        {
+            _owner = owner;
+        }
+
+        public void AddResourceFromDisk()
+        {
+            AddResourceFromDisk("All files (*.*)|*.*");
+        }
+
+        public void AddResourceFromDisk(string filter)
+        {
+            AddResourceFromDisk(filter, (files) =>
+                                    {
+                                        foreach (var file in files)
+                                        {
+                                            UploadFileForUse(file);
+                                        }
+                                    });
+        }
+        
+        private void AddResourceFromDisk(string filter, Action<IEnumerable<string>> withResources)
+        {
+            string initialDirectory = "c:\\";
+            foreach (var path in new[] { Environment.SpecialFolder.MyPictures, Environment.SpecialFolder.MyDocuments, Environment.SpecialFolder.DesktopDirectory, Environment.SpecialFolder.MyComputer })
+                try
+                {
+                    initialDirectory = Environment.GetFolderPath(path);
+                    break;
+                }
+                catch (Exception)
+                {
+                }
+            var fileBrowser = new OpenFileDialog
+                                         {
+                                             InitialDirectory = initialDirectory,
+                                             Filter = filter,
+                                             FilterIndex = 1,
+                                             RestoreDirectory = true
+                                         };
+            var dialogResult = fileBrowser.ShowDialog(_owner);
+
+            if (dialogResult ?? false)
+                withResources(fileBrowser.FileNames);
+        }
+
+        private const int fileSizeLimit = 50;
+        private void UploadFileForUse(string unMangledFilename)
+        {
+            string filename = unMangledFilename + ".MeTLFileUpload";
+            if (filename.Length > 260)
+            {
+                MeTLMessage.Information("Sorry, your filename is too long, must be less than 260 characters");
+                return;
+            }
+            if (FileHelper.LocalFileLessThanSizeInMegabytes(unMangledFilename, fileSizeLimit))
+            {
+                var worker = new BackgroundWorker();
+                worker.DoWork += (s, e) =>
+                 {
+                     var target = "presentationSpace"; // looks like this can be "presentationSpace" or "notepad"
+                     System.IO.File.Copy(unMangledFilename, filename);
+                     MeTLLib.ClientFactory.Connection().UploadAndSendFile(
+                         new MeTLStanzas.LocalFileInformation(Globals.slide, Globals.me, target, "public", filename, System.IO.Path.GetFileNameWithoutExtension(filename), false, new System.IO.FileInfo(filename).Length, SandRibbonObjects.DateTimeFactory.Now().Ticks.ToString()));
+                     System.IO.File.Delete(filename);
+                 };
+                worker.RunWorkerCompleted += (s, a) => _owner.Dispatcher.Invoke(DispatcherPriority.Send,
+                                                                                   (Action)(() => MeTLMessage.Information(string.Format("Finished uploading {0}.", unMangledFilename))));
+                worker.RunWorkerAsync();
+            }
+            else
+            {
+                MeTLMessage.Information(String.Format("Sorry your file is too large, it must be less than {0}mb", fileSizeLimit));
+                return;
+            }
+        }
+    }
+}
