d3c1aba5b74a835553d67d39154469524a9103a8
josh <devnull@localhost>
2012-01-27 16:52:14 +1100

    Updated build scripts to include grabbing source code
    Former-commit-id: 8aa5a8ba63a5864534600783741f7b62b5886e9c

--- MeTLMeeting/buildprod.bat
+++ MeTLMeeting/buildprod.bat
@@ -1,6 +1,6 @@
 @echo off
 echo **********************************
-echo * Building MeTL Production
+echo * MeTL Production BuildScript
 echo **********************************
 echo.
 echo Setting ApplicationRevision to %1
@@ -8,9 +8,20 @@ echo.
 
 IF "%1"=="" GOTO ERROR
 
+echo Grabbing latest from source control
+echo.
+
+hg pull
+hg update -C
+
+echo Building...
+echo.
+
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild.exe MeTL.sln /l:FileLogger,Microsoft.Build.Engine;logfile=MeTLBuildLog.log /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Build;Publish
 GOTO :EOF
+
+echo Done.
 
 :ERROR
 echo BuildScript Help v0.1b
