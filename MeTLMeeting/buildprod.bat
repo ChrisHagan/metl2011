fdd8e9a12c480ee35ca70508ed0d9a5e6215fe25
josh <joshua.jordan@monash.edu>
2012-01-27 16:30:16 +1100

    Build scripts working property, specify the build revision on the command line
    Former-commit-id: 0fe8d9101a5bafa8a7ee1dfc08dc1582ab8fa62a

--- MeTLMeeting/buildprod.bat
+++ MeTLMeeting/buildprod.bat
@@ -2,18 +2,18 @@
 echo **********************************
 echo * Building MeTL Production
 echo **********************************
-echo Setting ApplicationRevision to "%1"
+echo.
+echo Setting ApplicationRevision to %1
 echo.
 
 IF "%1"=="" GOTO ERROR
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-msbuild.exe MeTL.sln /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Publish
-GOTO END
+msbuild.exe MeTL.sln /l:FileLogger,Microsoft.Build.Engine;logfile=MeTLBuildLog.log /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Build;Publish
+GOTO :EOF
 
 :ERROR
 echo BuildScript Help v0.1b
 echo.
-echo "%0" <version number>
-
-:END
+echo %0 [version number]
+echo.
