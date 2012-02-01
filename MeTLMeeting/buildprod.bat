f79c85a67437de05447319436902ea83197d8807
josh <joshua.jordan@monash.edu>
2012-02-01 16:53:22 +1100

    Updated build scripts
    Former-commit-id: e554ed472013480e4b5db8a5d102672f195c232b

--- MeTLMeeting/buildprod.bat
+++ MeTLMeeting/buildprod.bat
@@ -18,13 +18,23 @@ echo Building...
 echo.
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-msbuild.exe MeTL.sln /l:FileLogger,Microsoft.Build.Engine;logfile=MeTLBuildLog.log /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Build;Publish
-GOTO :EOF
+IF /I "%2"=="nopublish" GOTO NOPUBLISH ELSE GOTO PUBLISH
 
+:PUBLISH
+msbuild.exe MeTL.sln /l:FileLogger,Microsoft.Build.Engine;logfile=MeTLBuildLog.log /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Build;Publish
+GOTO SUCCESS
+
+:NOPUBLISH
+msbuild.exe MeTL.sln /l:FileLogger,Microsoft.Build.Engine;logfile=MeTLBuildLog.log /p:Configuration=Release /p:Platform="Any CPU" /p:ApplicationRevision=%1 /t:Clean;Build
+GOTO SUCCESS
+
+:SUCCESS
+echo.
 echo Done.
+GOTO :EOF
 
 :ERROR
 echo BuildScript Help v0.1b
 echo.
-echo %0 [version number]
+echo %0 [version number] [nopublish]
 echo.
