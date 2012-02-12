c7f354ce0fe89d1f9893a4aaa4e711909cdaa3c0
josh <joshua.jordan@monash.edu>
2012-02-13 10:31:22 +1100

    Added missing file and updated proj
    Former-commit-id: 5f86d1009809be7a66b3c6ee6cb688d36a14dc40

---
+++ MeTLMeeting/FunctionalTests/Utilities/MeTLComponentHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Automation;
+using Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SandRibbon.Components;
+using UITestFramework;
+
+namespace FunctionalTests.Utilities
+{
+    public abstract class MeTLScreenComponent
+    {
+    }
+
+    public class ConversationAction : MeTLScreenComponent
+    {
+        public void WaitUntilConversationJoined(AutomationElement parent)
+        {
+            // wait until we've finished joining the conversation before returning
+            var canvasStack = new UITestHelper(parent, parent.Descendant(typeof(SandRibbon.Components.CollapsedCanvasStack)));
+            var success = canvasStack.WaitForControlEnabled();
+
+            Assert.IsTrue(success, ErrorMessages.WAIT_FOR_CONTROL_FAILED);
+        }
+    }
+}
