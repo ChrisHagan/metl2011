f2d434768c8b10e60148f3aee8db9f35e732d1f2
josh <joshua.jordan@monash.edu>
2012-04-18 09:36:17 +0100

    added missing file
    Former-commit-id: a3bf467c75fb4a0e9f3633cfbbf7d16c033fc519

---
+++ MeTLMeeting/MeTLLib/XElementExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MeTLLib
+{
+    public static class XElementExtensions
+    {
+        public static string ValueOrDefault(this XElement xelement, string defaultValue)
+        {
+            if (xelement == null)
+            {
+                return defaultValue;
+            }
+
+            return xelement.Value;
+        }
+    }
+}
