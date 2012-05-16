40515a8aa95606ce797bfc5ef4003233adebfe48
josh <devnull@localhost>
2012-05-16 11:44:53 +1000

    Added deploy script for prod testing
    Former-commit-id: fc689a289deee5987d53f1bd169c507b1835de94

---
+++ MeTLMeeting/DeployProdTestingMeTLClickonce.bat
@@ -0,0 +1,24 @@
+@echo off
+echo make sure you've already "published" from within VS2008.
+echo
+echo (make sure that when you published, you changed:
+echo SandRibbon - Properties - Publish -
+echo "Installation Folder URL"
+echo to: http://metl.adm.monash.edu/MeTLProdTesting/
+echo Updates -
+echo "Update Location"
+echo to: http://metl.adm.monash.edu/MeTLProdTesting/
+echo Options -
+echo Product name:
+echo MeTL Presenter
+echo Suite name:
+echo {leave blank}
+echo
+echo Don't use this command unless you're ready to overwrite 
+echo the currently deployed Clickonce on the server (metl.adm) 
+echo Press Ctrl+C to exit this.  Any other keypress will continue
+pause
+pscp.exe -r -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\Application Files" deploy@refer.adm.monash.edu.au:/srv/racecarDeploy/MeTLProdTesting/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\index.html" deploy@refer.adm.monash.edu.au:/srv/racecarDeploy/MeTLProdTesting/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\MeTL Presenter.application" deploy@refer.adm.monash.edu.au:/srv/racecarDeploy/MeTLProdTesting/
+pscp.exe -pw bananaman "C:\specialMeTL\MeTLMeeting\SandRibbon\publish\setup.exe" deploy@refer.adm.monash.edu.au:/srv/racecarDeploy/MeTLProdTesting/
