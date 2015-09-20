2ceef24087aef99db115265b9b78229702fbde89
Chris Hagan <captain.mister.hagan@gmail.com>
2015-09-20 21:10:05 +1000

    collaborationcontexts are now collected

--- MeTLMeeting/SandRibbon/Pages/Collaboration/ChooseCollaborationContextPage.xaml.cs
+++ MeTLMeeting/SandRibbon/Pages/Collaboration/ChooseCollaborationContextPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SandRibbon.Pages.ConversationSearch;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -10,11 +11,28 @@ namespace SandRibbon.Pages.Collaboration
         public ChooseCollaborationContextPage()
         {
             InitializeComponent();
-        }
-
-        private void GoToClass(object sender, RoutedEventArgs e)
+            collaborationContexts.ItemsSource = new List<CollaborationContext>
+            {
+                new CollaborationContext { code=1, label="I want to go to class",image=(Canvas)TryFindResource("appbar_user")},
+                new CollaborationContext { code=2, label="I'm looking for someone",image=(Canvas)TryFindResource("appbar_user_add")},
+                new CollaborationContext { code=3, label="I just need help",image=(Canvas)TryFindResource("appbar_book_perspective_help")}
+            };
+            collaborationContexts.SelectedIndex = 0;
+        }        
+        private void connect_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new ConversationSearchPage());
+            var selection = (CollaborationContext) collaborationContexts.SelectedItem;
+            switch (selection.code) {
+                case 1: NavigationService.Navigate(new ConversationSearchPage());
+                    break;
+                default: NavigationService.Navigate(new ConversationSearchPage());
+                    break;
+            }
         }
+    }
+    public class CollaborationContext {
+        public string label { get; set; }
+        public Canvas image { get; set; }
+        public int code { get; set; }
     }
 }
