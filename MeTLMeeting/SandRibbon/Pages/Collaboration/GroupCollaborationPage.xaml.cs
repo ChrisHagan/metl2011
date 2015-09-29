e9deaf55e4cdc96867b32553876e32ba19a1878c
Chris Hagan <captain.mister.hagan@gmail.com>
2015-09-29 23:14:01 +1000

    Incorporated changes into groupcollaboration page as well

--- MeTLMeeting/SandRibbon/Pages/Collaboration/GroupCollaborationPage.xaml.cs
+++ MeTLMeeting/SandRibbon/Pages/Collaboration/GroupCollaborationPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using SandRibbon.Components;
+﻿using Itschwabing.Libraries.ResourceChangeEvent;
+using SandRibbon.Components;
 using SandRibbon.Components.Utility;
+using SandRibbon.Pages.Collaboration.Palettes;
 using SandRibbon.Providers;
 using System;
 using System.Linq;
@@ -116,6 +118,32 @@ namespace SandRibbon.Pages.Collaboration
             var currentZoomWidth = scroll.ActualWidth / canvasViewBox.ActualWidth;
             var currentZoom = Math.Max(currentZoomHeight, currentZoomWidth);
             Commands.ZoomChanged.Execute(currentZoom);
+        }
+
+        private void ButtonWidthChanged(object sender, Itschwabing.Libraries.ResourceChangeEvent.ResourceChangeEventArgs e)
+        {
+            var behaviour = sender as ResourceChangeEventBehavior;
+            var element = behaviour.GetAssociatedObject();
+            if (element == null) return;
+            var context = element.DataContext as Bar;
+            if (context.Orientation == Orientation.Vertical)
+            {
+                var width = (Double)e.NewValue;
+                element.Width = width;
+            }
+        }
+
+        private void ButtonHeightChanged(object sender, Itschwabing.Libraries.ResourceChangeEvent.ResourceChangeEventArgs e)
+        {
+            var behaviour = sender as ResourceChangeEventBehavior;
+            var element = behaviour.GetAssociatedObject();
+            if (element == null) return;
+            var context = element.DataContext as Bar;
+            if (context.Orientation == Orientation.Horizontal)
+            {
+                var height = (Double)e.NewValue;
+                element.Height = height;
+            }
         }
     }
 }
