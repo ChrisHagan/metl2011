290e87cfe101e93578545ce31378aa84141c4fb1
Chris Hagan <captain.mister.hagan@gmail.com>
2015-10-02 14:26:26 +1000

    simplifying groupcollab interface

--- MeTLMeeting/SandRibbon/Pages/Collaboration/GroupCollaborationPage.xaml.cs
+++ MeTLMeeting/SandRibbon/Pages/Collaboration/GroupCollaborationPage.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using Itschwabing.Libraries.ResourceChangeEvent;
-using SandRibbon.Components;
-using SandRibbon.Components.Utility;
 using SandRibbon.Pages.Collaboration.Palettes;
 using SandRibbon.Providers;
 using System;
-using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 
 namespace SandRibbon.Pages.Collaboration
 {
@@ -18,108 +13,9 @@ namespace SandRibbon.Pages.Collaboration
         {
             InitializeComponent();
             this.slide = slide;
-            Bars.ItemsSource = Globals.currentProfile.castBars;
+            Bars.DataContext = Globals.currentProfile;
         }
-
-        private void zoomConcernedControlSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            UpdatePrivacyAdorners(adornerScroll.Target);
-            BroadcastZoom();
-        }
-
-        private void scroll_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            UpdatePrivacyAdorners(adornerScroll.Target);
-            BroadcastZoom();
-        }
-        private void UpdatePrivacyAdorners(string targetName)
-        {
-            if (RemovePrivacyAdorners(targetName))
-                try
-                {
-                    var lastValue = Commands.AddPrivacyToggleButton.LastValue();
-                    if (lastValue != null)
-                        AddPrivacyButton((PrivacyToggleButton.PrivacyToggleButtonInfo)lastValue);
-                }
-                catch (NotSetException) { }
-        }
-
-        private bool LessThan(double val1, double val2, double tolerance)
-        {
-            var difference = val2 * tolerance;
-            return val1 < (val2 - difference) && val1 < (val2 + difference);
-        }
-        private bool GreaterThan(double val1, double val2, double tolerance)
-        {
-            var difference = val2 * tolerance;
-            return val1 > (val2 - difference) && val1 > (val2 + difference);
-        }
-
-        private void AddPrivacyButton(PrivacyToggleButton.PrivacyToggleButtonInfo info)
-        {
-            Viewbox viewbox = null;
-            UIElement container = null;
-            GetViewboxAndCanvasFromTarget(info.AdornerTarget, out viewbox, out container);
-            Dispatcher.adoptAsync(() =>
-            {
-                var adornerRect = new Rect(container.TranslatePoint(info.ElementBounds.TopLeft, viewbox), container.TranslatePoint(info.ElementBounds.BottomRight, viewbox));
-                if (LessThan(adornerRect.Right, 0, 0.001) || GreaterThan(adornerRect.Right, viewbox.ActualWidth, 0.001)
-                    || LessThan(adornerRect.Top, 0, 0.001) || GreaterThan(adornerRect.Top, viewbox.ActualHeight, 0.001))
-                    return;
-                var adornerLayer = AdornerLayer.GetAdornerLayer(viewbox);
-                adornerLayer.Add(new UIAdorner(viewbox, new PrivacyToggleButton(info, adornerRect)));
-            });
-        }
-
-        private Adorner[] GetPrivacyAdorners(Viewbox viewbox, out AdornerLayer adornerLayer)
-        {
-            adornerLayer = AdornerLayer.GetAdornerLayer(viewbox);
-            if (adornerLayer == null)
-                return null;
-
-            return adornerLayer.GetAdorners(viewbox);
-        }
-
-        private bool RemovePrivacyAdorners(string targetName)
-        {
-            Viewbox viewbox;
-            UIElement container;
-            GetViewboxAndCanvasFromTarget(targetName, out viewbox, out container);
-
-            bool hasAdorners = false;
-            AdornerLayer adornerLayer;
-            var adorners = GetPrivacyAdorners(viewbox, out adornerLayer);
-            Dispatcher.adopt(() =>
-            {
-                if (adorners != null && adorners.Count() > 0)
-                {
-                    hasAdorners = true;
-                    foreach (var adorner in adorners)
-                        adornerLayer.Remove(adorner);
-                }
-            });
-
-            return hasAdorners;
-        }
-
-        private void GetViewboxAndCanvasFromTarget(string targetName, out Viewbox viewbox, out UIElement container)
-        {
-            if (targetName == "presentationSpace")
-            {
-                viewbox = canvasViewBox;
-                container = canvas;
-                return;
-            }         
-            throw new ArgumentException(string.Format("Specified target {0} does not match a declared ViewBox", targetName));
-        }
-        private void BroadcastZoom()
-        {
-            var currentZoomHeight = scroll.ActualHeight / canvasViewBox.ActualHeight;
-            var currentZoomWidth = scroll.ActualWidth / canvasViewBox.ActualWidth;
-            var currentZoom = Math.Max(currentZoomHeight, currentZoomWidth);
-            Commands.ZoomChanged.Execute(currentZoom);
-        }
-
+        
         private void ButtonWidthChanged(object sender, Itschwabing.Libraries.ResourceChangeEvent.ResourceChangeEventArgs e)
         {
             var behaviour = sender as ResourceChangeEventBehavior;
@@ -145,5 +41,8 @@ namespace SandRibbon.Pages.Collaboration
                 element.Height = height;
             }
         }
+    }
+    public class CollaborationViewModel {        
+
     }
 }
