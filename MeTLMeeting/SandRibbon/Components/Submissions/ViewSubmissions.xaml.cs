440c4053d162c2c66a9fb6a35673fb6e6261f696
Chris Hagan <captain.mister.hagan@gmail.com>
2015-12-20 22:34:23 +1100

    got selection working on submissions

--- MeTLMeeting/SandRibbon/Components/Submissions/ViewSubmissions.xaml.cs
+++ MeTLMeeting/SandRibbon/Components/Submissions/ViewSubmissions.xaml.cs
@@ -9,16 +9,27 @@ using System.Windows.Controls;
 using SandRibbon.Pages;
 using MeTLLib.Providers.Connection;
 using System.Linq;
+using System.Diagnostics;
 
 namespace SandRibbon.Components.Submissions
 {
-    public class DisplayableSubmission
+    public class DisplayableSubmission : DependencyObject
     {
         public BitmapImage Image { get; set; }
         public string Author { get; set; }
         public string Message { get; set; }
         public string Date { get; set; }
-        public bool IsSelected { get; set; } = false;
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public Uri Url { get; internal set; }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(DisplayableSubmission), new PropertyMetadata(false));
     }
     public partial class ViewSubmissions : Page
     {
@@ -27,27 +38,31 @@ namespace SandRibbon.Components.Submissions
         public ViewSubmissions(ConversationAwarePage rootPage)
         {
             InitializeComponent();
-            this.rootPage = rootPage;            
+            this.rootPage = rootPage;
             Submissions.DataContext = submissions;
             var receiveLiveScreenshot = new DelegateCommand<TargettedSubmission>(recieveSubmission);
-            var displaySubmissions = new DelegateCommand<object>(importAllSubmissionsInBucket, canImportSubmission);            
+            var displaySubmissions = new DelegateCommand<object>(importAllSubmissionsInBucket, canImportSubmission);
             Loaded += delegate
             {
                 Commands.ReceiveScreenshotSubmission.RegisterCommand(receiveLiveScreenshot);
                 Commands.ImportSubmissions.RegisterCommand(displaySubmissions);
-                rootPage.NetworkController.client.historyProvider.Retrieve<PreParser>(delegate { },delegate { },parser => {
-                    foreach (var s in parser.submissions) {
+                rootPage.NetworkController.client.historyProvider.Retrieve<PreParser>(delegate { }, delegate { }, parser =>
+                {
+                    foreach (var s in parser.submissions)
+                    {
                         submissions.Add(load(s));
                     }
-                },rootPage.ConversationDetails.Jid);
+                }, rootPage.ConversationDetails.Jid);
             };
-            Unloaded += delegate {
+            Unloaded += delegate
+            {
                 Commands.ReceiveScreenshotSubmission.UnregisterCommand(receiveLiveScreenshot);
                 Commands.ImportSubmissions.UnregisterCommand(displaySubmissions);
-            };                       
+            };
         }
 
-        private DisplayableSubmission load(TargettedSubmission submission) {
+        private DisplayableSubmission load(TargettedSubmission submission)
+        {
             var uri = rootPage.NetworkController.config.getResource(submission.url);
             var bitmap = new BitmapImage();
             bitmap.BeginInit();
@@ -56,12 +71,13 @@ namespace SandRibbon.Components.Submissions
             return new DisplayableSubmission
             {
                 Image = bitmap,
+                Url = uri,
                 Author = submission.author,
                 Message = submission.title,
                 Date = submission.timestamp.ToString()
             };
-        }    
-        
+        }
+
         private void recieveSubmission(TargettedSubmission submission)
         {
             if (String.IsNullOrEmpty(submission.target) || submission.target != "submission") return;
@@ -78,10 +94,10 @@ namespace SandRibbon.Components.Submissions
             var height = 0;
             foreach (var elem in items)
             {
-                var image = (TargettedSubmission)elem;
+                var image = (DisplayableSubmission)elem;
                 imagesToDrop.Add(new ImageDrop
                 {
-                    Filename = image.url.ToString(),
+                    Filename = image.Url.ToString(),
                     Target = "presentationSpace",
                     Point = new Point(0, height),
                     Position = 1,
@@ -98,10 +114,17 @@ namespace SandRibbon.Components.Submissions
 
         private void Submissions_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (e.AddedItems.Count > 0) {
+            if (e.AddedItems.Count > 0)
+            {
                 var submission = e.AddedItems[0] as DisplayableSubmission;
                 preview.Source = submission.Image;
             }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.TraceInformation(String.Join(",", submissions.Select(s => s.IsSelected.ToString())));
+            Commands.RequerySuggested(Commands.ImportSubmissions);
         }
     }
 }
